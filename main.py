e1c5f9494cdc595277970dbc7e295445ebbb8ab0
vanerk03 <vanerk4@gmail.com>
2022-12-25 22:47:56 +0300

    first version

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+from model import LinearRegressor
+from generate import generate
+import numpy as np
 
 def main():
-    pass
-
-if __name__ == "__main__":
-    main()
     """
     unlike in regular regression, here as a result we have a vector for EACH entry instead of a number, that means
     we should devise our own Gradient Descent for that problem 
     """
+    system = np.array([[1, 2], [3, 5]])
+    X, y = generate(system, dataset_size=500, scope=12)
+
+    model = LinearRegressor(system.shape, max_iter=300)
+    print(model.fit_predict(X, y))
+
+
+if __name__ == "__main__":
+    main()
+
--- model.py
+++ model.py
@@ -4,45 +4,59 @@ import numpy as np
 
 class BaseRegressor(ABC):
     @abstractmethod
-    def predict(self, X):
+    def fit_predict(self, X):
         pass
-
-    @abstractmethod
-    def fit(self, X, y) -> 'BaseRegressor':
-        pass
-    
-    @abstractmethod
-    def calc_loss(self, X, y):
-        pass 
 
 class LinearRegressor(BaseRegressor):
-    def __init__(self, system_size: tuple[int, int], max_iter: int=100, lr: float=0.001, tolerance: float=0.001) -> None:
+    """
+    Don't need fit and predict here separatelly
+    """
+    def __init__(self, system_size: tuple[int, int], max_iter: int=100, lr: float=0.003, tolerance: float=0.001) -> None:
         self.w = np.random.randn(*system_size)
         self._max_iter = max_iter
         self._lr: float = lr
         self._tolerance = tolerance
+        self._l1 = 0.05
+        self._l2 = 0.01
+        self._batch_size = 64
         # prolly regularization (although not sure if needed)
-        # prolly add decaying lr
+        # prolly add decaying learning rate
 
-    def calc_loss(self, X, y):
+    def _calc_loss(self, X, y):
         # for now I think mse is viable, although I begin to doubt it as I'm writing this))))
-        return np.sum(np.square(X @ self.predict(X).T - y))
+        return np.sum(np.abs(X @ self.w.T - y)) + self._l1 * np.sum(np.abs(self.w)) + self._l2 * np.sum(np.square(self.w))
 
     def _calc_grad(self, X, y):
-        pass
+        h = 0.00001
+        
+        idx = np.random.randint(0, X.shape[0], self._batch_size)
+        X, y = X[idx], y[idx]
+
+        grad = np.zeros(self.w.shape)
+        # couldn't find it by hand cuz of the hadamard square((((((
+        for i in range(self.w.shape[0]):
+            for j in range(self.w.shape[1]):
+                self.w[i][j] -= h
+                left_loss = self._calc_loss(X, y)
+                self.w[i][j] += 2 * h
+                right_loss = self._calc_loss(X, y)
+                self.w[i][j] -= h
+                grad[i][j] = (right_loss - left_loss)
+
+        return grad / X.shape[0] / h / 2
 
     def _step(self, X, y):
+        print("grad:\n", self._calc_grad(X, y))
         self.w -= self._lr * self._calc_grad(X, y)
         
-    def predict(self, X):
-        return self.w
-
-    def fit(self, X, y):
+    def fit_predict(self, X, y):
         prev_loss = np.inf
         for _ in range(self._max_iter):
-            self._step()
-            loss = self.calc_loss(X, y)
-            if loss - prev_loss < self._tolerance:
+            self._step(X, y)
+            print("coef:\n", self.w)
+            loss = self._calc_loss(X, y)
+            print(f"loss:\n {loss: .4f}")
+            if abs(loss - prev_loss) < self._tolerance:
                 break
             prev_loss = loss
-        return self
+        return self.w
